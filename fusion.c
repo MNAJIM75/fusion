f2185307974f1d79f179f284972a1bacd1a70031
MNAJIM75 <mnajim75@hotmail.com>
2026-03-14 06:13:24 +0200

    fusion v0.1

--- fusion.c
+++ fusion.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
 
-int main(void) {
+int luaopen_raylua(lua_State* L);
+int test_compiler(int argc, const char** argv); 
+int build_compiler(int argc, const char** argv); 
 
-	printf("Gundam is awesome\n");
+int main(int argc, const char** argv) {
+	return test_compiler(argc, argv);
+}
+
+int test_compiler(int argc, const char** argv) {
+	lua_State* L = luaL_newstate();
+	luaL_openlibs(L);
+	luaopen_raylua(L);
+	if (luaL_dofile(L, "test.lua") != LUA_OK) {
+		fprintf(stderr, "Lua error: %s\n", lua_tostring(L, -1));
+	}
+	lua_close(L);
+	return 0;
+}
+
+int build_compiler(int argc, const char** argv) {
+	lua_State* L = luaL_newstate();
+	luaL_openlibs(L);
+	lua_newtable(L);
+	int i = 0;
+	while (argc != i) {
+	  lua_pushstring(L, argv[i]);
+	  lua_rawseti(L, -2, i);
+	  i++;
+	}
+	lua_setglobal(L, "arg");
+	luaopen_raylua(L);
+	// if (luaL_dofile(L, "test.lua") != LUA_OK) {
+	// 	fprintf(stderr, "Lua error: %s\n", lua_tostring(L, -1));
+	// }
+	lua_close(L);
 	return 0;
 }
--- makefile
+++ makefile
@@ -1,3 +1,12 @@
-fusion: fusion.c
-	gcc -o $@ $^
+CFLAGS := -O2 -s
+LDFLAGS := -O2 -s -lm
+CFLAGS += -Iraylib-lua/luajit/src -Iraylib-lua/raylib/src -Iraylib-lua/raygui/src -Iraylib-lua/physac/src
+LDFLAGS += raylib-lua/libraylua.a raylib-lua/luajit/src/libluajit.a raylib-lua/raylib/src/libraylib.a 
 
+AR ?= ar
+LUA ?= raylib-lua/luajit/src/luajit
+
+LDFLAGS += -lopengl32 -lgdi32 -lwinmm -static
+
+fusion: fusion.c
+	gcc -o $@ $(CFLAGS) $^ $(LDFLAGS)
