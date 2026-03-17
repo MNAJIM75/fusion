a1564ccbe7dfadd85518a3c7cba12bb653447624
MNAJIM75 <mnajim75@hotmail.com>
2026-03-17 05:06:00 +0200

    async-ish

--- test.lua
+++ test.lua
@@ -1,9 +1,39 @@
-client_init()
+setmetatable(_G, {__index=rl})
 
-for i=1, 10 do
-    client_send("Hello")
-    local recv_msg = client_recv()
-    if recv_msg then print(recv_msg) end
+InitWindow(800, 600, "Kate")
+SetTargetFPS(60)
+
+function client_task()
+    client_init()
+    while true do
+        client_send("Hello")
+        coroutine.yield()  -- pause here, resume later
+        local recv_msg = client_recv()
+        if recv_msg then
+            print(recv_msg)
+        end
+
+    end
+    client_close()
 end
 
-client_close()
+-- Create coroutine
+co = coroutine.create(client_task)
+
+local x, y = 10, 10
+while not WindowShouldClose() and coroutine.status(co) ~= "dead" do
+    -- update
+    coroutine.resume(co)
+    x = x + 10 * GetFrameTime()
+    y = y + 10 * GetFrameTime()
+    -- draw
+    BeginDrawing()
+    ClearBackground(WHITE)
+
+    DrawFPS(10, 10)
+    DrawCircle(x, y, 10, RED)
+
+    EndDrawing()
+end
+if coroutine.status(co) ~= "dead" then client_close() end
+CloseWindow()
